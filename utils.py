bdd388b177d60ab0638fcb0f10dde94e004f3f39
geonseoks <geonseoks@snu.ac.kr>
2018-12-31 14:48:25 +0900

    Calculate box & anchor coordinates

---
+++ utils.py
@@ -0,0 +1,38 @@
+# ----------------------------------------------------------------
+# Tensorflow SSD
+#
+# Written by Geonseok Seo, based on code from SSD by Lukasz Janyst
+# ----------------------------------------------------------------
+from collections import namedtuple
+
+Label   = namedtuple('Label',   ['name', 'color'])
+Size    = namedtuple('Size',    ['w', 'h'])
+Point   = namedtuple('Point',   ['x', 'y'])
+Sample  = namedtuple('Sample',  ['filename', 'boxes', 'imgsize'])
+Box     = namedtuple('Box',     ['label', 'labelid', 'center', 'size'])
+Score   = namedtuple('Score',   ['idx', 'score'])
+Overlap = namedtuple('Overlap', ['best', 'good'])
+
+def abs2prop(xmin, xmax, ymin, ymax, imgsize):
+    """
+    Convert the absolute min-max box bound to proportional center-width bounds
+    """
+    width   = float(xmax-xmin)
+    height  = float(ymax-ymin)
+    cx      = float(xmin)+width/2.0
+    cy      = float(ymin)+height/2.0
+    width  /= imgsize.w
+    height /= imgsize.h
+    cx     /= imgsize.w
+    cy     /= imgsize.h
+    return Point(cx, cy), Size(width, height)
+
+def prop2abs(center, size, imgsize):
+    """
+    Convert proportional center-width bounds to absolute min-max bounds
+    """
+    width2  = size.w*imgsize.w/2.0
+    height2 = size.h*imgsize.h/2.0
+    cx      = center.x*imgsize.w
+    cy      = center.y*imgsize.h
+    return int(cx-width2), int(cx+width2), int(cy-height2), int(cy+height2)
